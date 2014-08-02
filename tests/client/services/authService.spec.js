fed659991b43c42b96ed2b2a7ba94de44c4826b4
Jinto Jose <jintoppy@gmail.com>
2014-08-02 22:59:39 +0530

    unit test update

--- tests/client/services/authService.spec.js
+++ tests/client/services/authService.spec.js
@@ -1,13 +1,15 @@
 describe('authService', function(){
-	var authService;
+	var authService, $rootScope;
 	beforeEach(module('quiz.services'));
 
-	beforeEach(inject(function(_authService_){
+	beforeEach(inject(function(_$rootScope_,_authService_){
 		authService = _authService_;
+		$rootScope = _$rootScope_;
 	}));
 
 	it('should have isAuthenticated defined', function(){
 		expect(authService.isAuthenticated).toBeDefined();
+		$rootScope.$digest();
 	});
 
 });
