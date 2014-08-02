b61a0f68776510910ef956bfdd7696af876edd8d
Jinto Jose <jintoppy@gmail.com>
2014-08-02 19:53:27 +0530

    finalizing the server setup

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express'),
+	path = require('path'),
+	consolidate = require('consolidate'),
 	bodyParser = require('body-parser');
 
 	var app = express();
-	app.use(bodyParser.urlencoded());
-	app.set('view engine', 'server.view.html');
-	app.set('views', './app/views');
-	app.use(express.static(path.resolve('./public')));
-	app.use(bodyParser.json());
 
-	app.listen(config.port);
+	app.use(bodyParser.urlencoded());
+	// Set swig as the template engine
+	app.engine('server.html', consolidate['swig']);
+	app.set('view engine', 'server.html');
+	app.set('views', './app/views');
+	app.use(bodyParser.json());
+	app.use(express.static(path.resolve('./public')));
+	
+	var hello = require('./app/routes.js')(app);
+	console.log(hello);
+
+	app.listen(process.env.PORT || 3800);
