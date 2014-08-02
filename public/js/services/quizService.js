af260befe83234cdda36af4c2de455b1c5d8f4c8
Jinto Jose <jintoppy@gmail.com>
2014-08-02 22:56:06 +0530

    adding the travis

--- public/js/services/quizService.js
+++ public/js/services/quizService.js
@@ -12,15 +12,30 @@ angular.module('quiz.services').
 		];
 
 
-		var getQuizData = function(){
+		function getQuizData(){
 			var deferred = $q.defer();
 			deferred.resolve(quizData);
 			
 			return deferred.promise;
-		};
+		}
+
+		function submitAnswer(inputData){
+
+		}
+
+		function getResult(){
+
+		}
+
+		function getQuestionsWithAnswers(){
+
+		}
 
 		return {
-			getQuizData: getQuizData
+			getQuizData: getQuizData,
+			submitAnswer: submitAnswer,
+			getResult: getResult,
+			getQuestionsWithAnswers: getQuestionsWithAnswers
 		};
 
 	}]);
